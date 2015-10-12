2e333f479cdf5a7ee249b6d24e277eccf45c2924
yusuke0128 <t.yuusuke0128@outlook.jp>
2015-10-12 14:05:42 +0900

    メディアンフィルタのプログラム

---
+++ MedianFilter.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import sys  
+  
+argvs=sys.argv  
+if (len(argvs) != 2):  
+    print 'Usage: # python %s filename' % argvs[0]  
+    quit()   
+  
+imagefilename = argvs[1]  
+try:  
+    img=cv2.imread(imagefilename, 0)  
+except:  
+    print 'faild to load %s' % imagefilename  
+    quit()  
+
+ksize =input('please ksize')
+median = cv2.medianBlur(img,ksize)
+MedianName = raw_input('please fileName after MedianFilter') 
+cv2.imwrite(MedianName,median)
