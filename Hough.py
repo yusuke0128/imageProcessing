b6a51d5a476a7246cf63ad55f5630c2c5547a112
yusuke0128 <t.yuusuke0128@outlook.jp>
2015-10-14 16:33:53 +0900

    不要な変数iの初期化を削除

--- Hough.py
+++ Hough.py
@@ -18,7 +18,7 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
 
 lines = cv2.HoughLines(edges,1,np.pi/180,200) 
-i=0
+
 for i in range(0,len(lines)): 
 
  for rho,theta in lines[i]:
