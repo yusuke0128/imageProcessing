3a17e4b282765c5ce0dc49cb85f405b410f1221d
yusuke0128 <t.yuusuke0128@outlook.jp>
2015-10-14 14:16:27 +0900

    古典的ハフ変換

---
+++ Hough.py
@@ -0,0 +1,38 @@
+import cv2 
+import numpy as np 
+import sys
+
+argvs=sys.argv  
+if (len(argvs) != 2):  
+    print 'Usage: # python %s filename' % argvs[0]  
+    quit()   
+  
+imagefilename = argvs[1]  
+try:  
+    img=cv2.imread(imagefilename)  
+except:  
+    print 'faild to load %s' % imagefilename  
+    quit()  
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+
+lines = cv2.HoughLines(edges,1,np.pi/180,200) 
+i=0
+for i in range(0,len(lines)): 
+
+ for rho,theta in lines[i]:
+    
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+HoughName = raw_input('please fileName after Hough') 
+cv2.imwrite(HoughName,img)
+
